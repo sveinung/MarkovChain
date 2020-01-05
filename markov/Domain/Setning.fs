e8e9c0cff2602e738eeba924893621fa79168f9e
Sveinung Dalatun <sdalatun@gmail.com>
2020-01-05 22:14:14 +0100

    Parsing av fleire setningar

--- markov/Domain/Setning.fs
+++ markov/Domain/Setning.fs
@@ -2,13 +2,19 @@ namespace Domain
 
 module Setning =
     let parseSetning (setningString: string) =
-        setningString.Split [|' '|]
+        setningString
+            .Trim()
+            .Split [| ' ' |]
             |> Array.toList
 
+    let ikkjeTomSetning (setning: list<string>) =
+        not (setning.Equals([""]))
+
     let public parseSetningar (tekst: string) =
-        let setningar = tekst.Split [|'.'|]
+        let setningar = tekst.Split [| '.' |]
         let result =
             setningar
             |> Array.map parseSetning
+            |> Array.filter ikkjeTomSetning
             |> Array.toList
         (result)
--- markov/DomainTest/SetningTest.fs
+++ markov/DomainTest/SetningTest.fs
@@ -11,10 +11,18 @@ let Setup() =
 [<Test>]
 let Parsing_av_enkeltsetningar() =
     let result = Setning.parseSetning "hei på deg"
-    Assert.That(result, Is.EqualTo(["hei"; "på"; "deg"]))
+    Assert.That(result, Is.EquivalentTo(["hei"; "på"; "deg"]))
 
 [<Test>]
 let Parsing_av_setningar() =
     let result = Setning.parseSetningar "hei på deg"
-    Assert.That(result, Is.EqualTo([["hei"; "på"; "deg"]]))
+    Assert.That(result, Is.EquivalentTo([["hei"; "på"; "deg"]]))
+
+[<Test>]
+let Parsing_av_to_setningar() =
+    let result = Setning.parseSetningar "hei på deg. Her er eg."
+    Assert.That(result, Is.EquivalentTo([
+        ["hei"; "på"; "deg"];
+        ["Her"; "er"; "eg"]
+    ]))
 
