3caad6e975e9a729f791b1215aff84a25012a709
Sveinung Dalatun <sdalatun@gmail.com>
2020-03-16 21:56:36 +0100

    Serialiser modellen til JSON

---
+++ markov/Persistance/MarkovChainFileRepository.fs
@@ -0,0 +1,40 @@
+module MarkovChainFileRepository
+
+open Newtonsoft.Json
+
+open Domain.MarkovChain
+
+type TransitionDto = {
+    Probability: double;
+    EndState: string;
+}
+
+let toJson (markovChain: MarkovChain) =
+    let mapTransition (transition: Transition): TransitionDto =
+        let endStateString =
+            match transition.endState with
+            | End -> "__END__"
+            | EndStateValue.State value -> value
+
+        { Probability = transition.probability;
+          EndState = endStateString}
+
+    let mapState ((_, state): StartStateValue * State): string * TransitionDto list =
+        let stateString =
+            match state.state with
+            | Start -> "__START__"
+            | StartStateValue.State value -> value
+
+        let transitionsJson =
+            state.transitions
+                |> List.map mapTransition
+
+        (stateString, transitionsJson)
+
+    let dto =
+        markovChain
+            |> Map.toList
+            |> List.map mapState
+            |> Map.ofList
+
+    JsonConvert.SerializeObject dto
---
+++ markov/PersistanceTest/MarkovChainFileRepositoryTest.fs
@@ -0,0 +1,29 @@
+module PersistanceTest.MarkovChainFileRepositoryTest
+
+open NUnit.Framework
+
+open Domain.MarkovChain
+
+[<Test>]
+let ``Map``() =
+    let s0: State =
+        { state = StartStateValue.Start;
+          transitions = [
+              { probability = 0.5;
+                endState = EndStateValue.State("2") };
+              { probability = 0.5;
+                endState = EndStateValue.State("1") }
+          ]
+        }
+    let s1: State =
+        { state = StartStateValue.State("1");
+          transitions = [
+              { probability = 0.5;
+                endState = EndStateValue.State("2") };
+              { probability = 0.5;
+                endState = EndStateValue.End }
+          ]
+        }
+    let chain = Map.empty.Add(s0.state, s0).Add(s1.state, s1)
+    let result = MarkovChainFileRepository.toJson chain
+    Assert.That(result, Is.EquivalentTo "{\"1\":[{\"Probability\":0.5,\"EndState\":\"2\"},{\"Probability\":0.5,\"EndState\":\"__END__\"}],\"__START__\":[{\"Probability\":0.5,\"EndState\":\"2\"},{\"Probability\":0.5,\"EndState\":\"1\"}]}")
