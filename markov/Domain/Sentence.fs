265f99a2fdcf91414cea94637a1df068e2099622
Sveinung Dalatun <sdalatun@gmail.com>
2020-03-21 20:16:51 +0100

    Refaktorering

--- markov/Domain/Sentence.fs
+++ markov/Domain/Sentence.fs
@@ -1,20 +1,17 @@
 namespace Domain
 
 module Sentence =
-    let parseSingleSentence (sentence: string) =
-        sentence
-            .Trim()
-            .Split [| ' ' |]
-            |> Array.toList
-
-    let notEmpty (sentence: list<string>) =
-        not (sentence.Equals([""]))
-
     let public parse (text: string): List<List<string>> =
-        let sentences = text.Split [| '.' |]
-        let result =
-            sentences
+        let parseSingleSentence (sentence: string) =
+            sentence
+                .Trim()
+                .Split [| ' ' |]
+                |> Array.toList
+
+        let notEmpty (sentence: list<string>) =
+            not (sentence.Equals([""]))
+
+        text.Split [| '.' |]
             |> Array.map parseSingleSentence
             |> Array.filter notEmpty
             |> Array.toList
-        (result)
--- markov/DomainTest/SentenceTest.fs
+++ markov/DomainTest/SentenceTest.fs
@@ -4,15 +4,6 @@ open NUnit.Framework
 
 open Domain
 
-[<SetUp>]
-let Setup() =
-    ()
-
-[<Test>]
-let Parsing_single_sentence() =
-    let result = Sentence.parseSingleSentence "hei på deg"
-    Assert.That(result, Is.EquivalentTo(["hei"; "på"; "deg"]))
-
 [<Test>]
 let Parsing_list_of_sentences() =
     let result = Sentence.parse "hei på deg"
