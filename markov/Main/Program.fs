20a3638b2a5f42db16de9276f69604b818acb193
Sveinung Dalatun <sdalatun@gmail.com>
2020-03-23 20:59:54 +0100

    Lagring av modellen

--- markov/Main/Program.fs
+++ markov/Main/Program.fs
@@ -7,7 +7,12 @@ open Domain.Transitions
 let createModel (sourceFilePath: string): unit =
     let concatTransitions (acc: Transition list) (elem: Transition list): Transition list =
         acc @ elem
-    
+
+    let save json =
+        let fileName = String.Format ("{0}.markovmodel.json", sourceFilePath)
+        File.WriteAllText (fileName, json)
+        Console.WriteLine ("Model written to " + fileName)
+
     printfn "Creating model from %s ..." sourceFilePath
     File.ReadLines sourceFilePath
         |> Seq.map Sentence.parse
@@ -16,8 +21,7 @@ let createModel (sourceFilePath: string): unit =
         |> List.fold concatTransitions List.empty<Transition>
         |> Transitions.toMarkovChain
         |> MarkovChainFileRepository.toJson
-        |> (fun x -> Console.WriteLine ("--- " + x))
-        |> ignore
+        |> save
     ()
 
 [<EntryPoint>]
